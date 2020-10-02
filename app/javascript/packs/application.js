c2d784d6b4b0c9ff0602f8551b19396b91612e32
Konstantin <konstantin.faleev@gmail.com>
2020-10-02 11:24:58 +0300

    fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("bootstrap")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,6 +17,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import $ from 'jquery';
+global. $ = $
+global.jQuery = $
+
 $(document).ready(function() {
     if ($('.pagination').length) {
         $(window).scroll(function() {
